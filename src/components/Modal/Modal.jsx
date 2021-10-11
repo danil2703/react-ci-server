eec212a6d627a997dca7422bee356b704a677ea9
danil2703 <d.lyashchenko@takeoff-staff.ru>
2021-10-11 13:36:57 +0300

    return close modal on ecsape

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Button } from '../Button/Button';
 import './Modal.scss';
@@ -9,11 +9,22 @@ export const Modal = React.memo((props) => {
 
   const { open, title, text, type } = modalData;
 
+  const onKeydown = useCallback(({ key }) => {
+    if (key === 'Escape') {
+      onClose();
+    }
+  });
+
+  useEffect(() => {
+    document.addEventListener('keydown', onKeydown);
+    return () => document.removeEventListener('keydown', onKeydown);
+  });
+
   if (!open) return null;
 
   return (
-    <div className="modal">
-      <div className="modal__dialog">
+    <div className="modal" onClick={onClose}>
+      <div className="modal__dialog" onClick={(e) => e.stopPropagation()}>
         <h3 className="modal__title">{title}</h3>
         <p className="modal__text">{text}</p>
         {type === 'error' ? (
